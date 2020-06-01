61766c95ef97d669771e446fd49687e4e38d5c88
outloudvi <19144373+outloudvi@users.noreply.github.com>
2020-06-01 06:37:13 +0000

    Deploying to gh-pages from  @ 57e3ab53044602e1c62ccd482f4871304d2197db 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-importScripts("https://cdn.jsdelivr.net/npm/workbox-sw@5.1.3/build/workbox-sw.min.js", "/precache-manifest.7e3587af054d1cc9b6839edd350e565b.js");
+importScripts("https://cdn.jsdelivr.net/npm/workbox-sw@5.1.3/build/workbox-sw.min.js", "/precache-manifest.8dbccaa51d72416da2352b61861f470f.js");
 
 workbox.setConfig({
   modulePathCb(name, debug) {
     const env = debug ? "dev" : "prod";
     return `https://cdn.jsdelivr.net/npm/${name}@5.1.3/build/${name}.${env}.js`;
-  },
+  }
 });
 
 const INITIAL_PRECACHING_URL = [
   "/assets/ehhh.mp3",
   "/assets/ehhh2.mp3",
-  "/assets/ehhh3.mp3",
+  "/assets/ehhh3.mp3"
 ];
 
 /**
  * @schema { revision, url }
  */
 workbox.precaching.precacheAndRoute(
-  self.__precacheManifest.filter((obj) => {
+  self.__precacheManifest.filter(obj => {
     INITIAL_PRECACHING_URL.includes(obj.url);
   }) || []
 );
@@ -29,9 +29,9 @@ workbox.routing.registerRoute(
     plugins: [
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 120,
-        maxAgeSeconds: 30 * 24 * 60 * 60,
-      }),
-    ],
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      })
+    ]
   })
 );
 
