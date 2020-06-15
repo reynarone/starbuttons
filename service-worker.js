a037a65dc8107bb2e998a946e2712e372e95fd03
outloudvi <19144373+outloudvi@users.noreply.github.com>
2020-06-15 15:38:41 +0000

    Deploying to gh-pages from  @ 926572b6fd06ae25e701b78929b3858a7fc282a6 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/npm/workbox-sw@5.1.3/build/workbox-sw.min.js", "/precache-manifest.d37959a9645874d2323b9da492cdc5ca.js");
+importScripts("https://cdn.jsdelivr.net/npm/workbox-sw@5.1.3/build/workbox-sw.min.js", "/precache-manifest.109ebbcba07d23aea4085ccaa56d7e72.js");
 
 workbox.setConfig({
   modulePathCb(name, debug) {
