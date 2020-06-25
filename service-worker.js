0806d797a12ec5d1c0c7ca1117cda209b60b78b9
outloudvi <19144373+outloudvi@users.noreply.github.com>
2020-06-25 17:37:34 +0000

    Deploying to gh-pages from  @ 841a4f4c40f24d41ebd854b76670bdaaccd7b4e3 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,10 @@
-importScripts("https://cdn.jsdelivr.net/npm/workbox-sw@5.1.3/build/workbox-sw.min.js", "/precache-manifest.51dee35926da7b3aa8beaa024f3560cd.js");
+importScripts("/precache-manifest.0cf0f572bbacd606dcb9914212dd8a8f.js");
 
-workbox.setConfig({
-  modulePathCb(name, debug) {
-    const env = debug ? "dev" : "prod";
-    return `https://cdn.jsdelivr.net/npm/${name}@5.1.3/build/${name}.${env}.js`;
-  }
+self.addEventListener("install", function(event) {
+  event.waitUntil(self.skipWaiting());
 });
 
-const INITIAL_PRECACHING_URL = [
-  "/assets/ehhh.mp3",
-  "/assets/ehhh2.mp3",
-  "/assets/ehhh3.mp3"
-];
-
-/**
- * @schema { revision, url }
- */
-workbox.precaching.precacheAndRoute(
-  self.__precacheManifest.filter(obj => {
-    INITIAL_PRECACHING_URL.includes(obj.url);
-  }) || []
-);
-
-workbox.routing.registerRoute(
-  /\.(?:mp3|ogg|webm|m4a)$/,
-  new workbox.strategies.CacheFirst({
-    cacheName: "audios",
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 120,
-        maxAgeSeconds: 30 * 24 * 60 * 60
-      })
-    ]
-  })
-);
+self.addEventListener("activate", function(event) {
+  event.waitUntil(self.clients.claim());
+});
 
