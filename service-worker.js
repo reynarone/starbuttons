809ffb1e70b396593d2866edca01f4313b4789eb
outloudvi <19144373+outloudvi@users.noreply.github.com>
2020-06-28 14:20:44 +0000

    Deploying to gh-pages from  @ 4b7194462451324bb0b0095f8e0f78edc7e557fd 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.d81527eb35370452c90c83a38c8b1460.js");
+importScripts("/precache-manifest.2c9b0ff2212e523ec7dae4dff6f1941d.js");
 
 self.addEventListener("install", function(event) {
   event.waitUntil(self.skipWaiting());
